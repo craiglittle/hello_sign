c8941b3b0f141920082a258caa8b397337399f5a
Craig Little <craiglttl@gmail.com>
2013-01-17 01:48:49 -0800

    Fix rspec configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
-    c.syntax = :expect
+    c.syntax = [:expect, :should]
   end
+
+  config.color_enabled = true
+  config.tty           = true
+  config.formatter     = :documentation
 end
