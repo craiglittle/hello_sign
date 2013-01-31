f858697988422071a1c71d9b0c1b7379722f3469
Craig Little <craiglttl@gmail.com>
2013-01-31 00:57:38 -0800

    Factor out body formatting duplication

--- lib/hello_sign/reusable_form_proxy.rb
+++ lib/hello_sign/reusable_form_proxy.rb
@@ -16,27 +16,23 @@ module HelloSign
     end
 
     def grant_access(form_id, params = {})
-      body = if email = params[:email]
-        {:email_address => email}
-      elsif account_id = params[:account_id]
-        {:account_id => account_id}
-      else
-        raise ArgumentError, 'An email address or account ID must be provided.'
-      end
-
-      client.post("/reusable_form/add_user/#{form_id}", :body => body)
+      client.post("/reusable_form/add_user/#{form_id}", :body => body(params))
     end
 
     def revoke_access(form_id, params = {})
-      body = if email = params[:email]
+      client.post("/reusable_form/remove_user/#{form_id}", :body => body(params))
+    end
+
+    private
+
+    def body(params)
+      if email = params[:email]
         {:email_address => email}
       elsif account_id = params[:account_id]
         {:account_id => account_id}
       else
         raise ArgumentError, 'An email address or account ID must be provided.'
       end
-
-      client.post("/reusable_form/remove_user/#{form_id}", :body => body)
     end
 
   end
