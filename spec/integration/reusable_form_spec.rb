7c099f977458e95a9db35e7a82f5654c1c7a4594
Craig Little <craiglttl@gmail.com>
2013-03-10 10:36:59 -0700

    Refactor reusable form integration specs

--- spec/integration/reusable_form_spec.rb
+++ spec/integration/reusable_form_spec.rb
@@ -2,34 +2,31 @@ require 'integration/helper'
 
 describe HelloSign do
   context "when getting a list of reusable forms" do
-    before do
-      stub_get_with_auth('/reusable_form/list')
-      HelloSign.reusable_form.list
-    end
+    before { stub_get_with_auth('/reusable_form/list') }
 
     it "fetches a list of reusable forms from the HelloSign API" do
+      HelloSign.reusable_form.list
+
       expect(a_get_with_auth('/reusable_form/list')).to have_been_made
     end
   end
 
   context "when fetching information about a reusable form" do
-    before do
-      stub_get_with_auth('/reusable_form/form_id')
-      HelloSign.reusable_form.show('form_id')
-    end
+    before { stub_get_with_auth('/reusable_form/form_id') }
 
     it "fetches the reusable form information from the HelloSign API" do
+      HelloSign.reusable_form.show('form_id')
+
       expect(a_get_with_auth('/reusable_form/form_id')).to have_been_made
     end
   end
 
   context "when giving a user access to a reusable form" do
-    before do
-      stub_post_with_auth('/reusable_form/add_user/form_id')
-      HelloSign.reusable_form.grant_access('form_id', :email_address => 'john@johnson.com')
-    end
+    before { stub_post_with_auth('/reusable_form/add_user/form_id') }
 
     it "sends a request to grant form access to the HelloSign API" do
+      HelloSign.reusable_form.grant_access('form_id', :email_address => 'john@johnson.com')
+
       expect(a_post_with_auth('/reusable_form/add_user/form_id')
         .with(:email_address => 'john@johnson.com')
       ).to have_been_made
@@ -37,12 +34,11 @@ describe HelloSign do
   end
 
   context "when taking away a user's access to a reusable form" do
-    before do
-      stub_post_with_auth('/reusable_form/remove_user/form_id')
-      HelloSign.reusable_form.revoke_access('form_id', :email_address => 'john@johnson.com')
-    end
+    before { stub_post_with_auth('/reusable_form/remove_user/form_id') }
 
     it "sends a request to grant form access to the HelloSign API" do
+      HelloSign.reusable_form.revoke_access('form_id', :email_address => 'john@johnson.com')
+
       expect(a_post_with_auth('/reusable_form/remove_user/form_id')
         .with(:email_address => 'john@johnson.com')
       ).to have_been_made
