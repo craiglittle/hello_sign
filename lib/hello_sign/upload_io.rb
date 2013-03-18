d67f3cd0bd5496411e862dc329596833248704c2
Craig Little <craiglttl@gmail.com>
2013-03-17 19:04:24 -0700

    Split out list of MIME types into separate file

--- lib/hello_sign/upload_io.rb
+++ lib/hello_sign/upload_io.rb
@@ -1,26 +1,8 @@
+require 'hello_sign/upload_io/mime_types'
 require 'faraday/upload_io'
-require 'hello_sign/error'
 
 module HelloSign
   class UploadIO
-    MIME_TYPES = {
-      'doc'  => 'application/msword',
-      'docx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-      'gif'  => 'image/gif',
-      'jpg'  => 'image/jpeg',
-      'jpeg' => 'image/jpeg',
-      'pdf'  => 'application/pdf',
-      'png'  => 'image/png',
-      'ppsx' => 'application/vnd.openxmlformats-officedocument.presentationml.slideshow',
-      'ppt'  => 'application/vnd.ms-powerpoint',
-      'pptx' => 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
-      'tif'  => 'image/tiff',
-      'tiff' => 'image/tiff',
-      'txt'  => 'text/plain',
-      'xls'  => 'application/vnd.ms-excel',
-      'xlsx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-    }
-
     attr_reader :filename, :io_object
 
     def initialize(file_data)
---
+++ lib/hello_sign/upload_io/mime_types.rb
@@ -0,0 +1,21 @@
+module HelloSign
+  class UploadIO
+    MIME_TYPES = {
+      'doc'  => 'application/msword',
+      'docx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+      'gif'  => 'image/gif',
+      'jpg'  => 'image/jpeg',
+      'jpeg' => 'image/jpeg',
+      'pdf'  => 'application/pdf',
+      'png'  => 'image/png',
+      'ppsx' => 'application/vnd.openxmlformats-officedocument.presentationml.slideshow',
+      'ppt'  => 'application/vnd.ms-powerpoint',
+      'pptx' => 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+      'tif'  => 'image/tiff',
+      'tiff' => 'image/tiff',
+      'txt'  => 'text/plain',
+      'xls'  => 'application/vnd.ms-excel',
+      'xlsx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    }
+  end
+end
