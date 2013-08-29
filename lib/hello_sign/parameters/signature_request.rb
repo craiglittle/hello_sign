b6d9cddcd6b6029998531b06a0aa78a64be07bc6
Craig Little <craiglttl@gmail.com>
2013-08-29 00:34:17 -0700

    Refactor signature request parameters
    Use #each_with_object in favor of #inject.

--- lib/hello_sign/parameters/signature_request.rb
+++ lib/hello_sign/parameters/signature_request.rb
@@ -3,8 +3,7 @@ require 'hello_sign/file'
 module HelloSign
   module Parameters
     class SignatureRequest
-      attr_accessor :title, :subject, :message, :ccs
-      attr_writer :signers, :files
+      attr_writer :title, :subject, :message, :ccs, :signers, :files
 
       def formatted
         {
@@ -12,30 +11,37 @@ module HelloSign
           subject:            subject,
           message:            message,
           cc_email_addresses: ccs,
-          signers:            signers,
-          file:               files
+          signers:            formatted_signers,
+          file:               formatted_files
         }
       end
 
       private
 
-      def signers
-        (@signers || {}).each_with_index.inject({}) do |parameter, (signer, index)|
-          signer = {
+      attr_reader :title, :subject, :message, :ccs
+
+      def formatted_signers
+        signers.each_with_index.each_with_object({}) do |(signer, i), parameter|
+          parameter[i] = {
             name:          signer[:name],
             email_address: signer[:email_address],
-            order:         index
+            order:         i
           }
-          parameter[index] = signer
-          parameter
         end
       end
 
-      def files
-        (@files || {}).each_with_index.inject({}) do |parameter, (file_data, index)|
-          parameter[index + 1] = HelloSign::File.new(file_data).attachment
-          parameter
+      def formatted_files
+        files.each_with_index.each_with_object({}) do |(file, i), parameter|
+          parameter[i + 1] = HelloSign::File.new(file).attachment
         end
+      end
+
+      def signers
+        @signers || {}
+      end
+
+      def files
+        @files || {}
       end
 
     end
